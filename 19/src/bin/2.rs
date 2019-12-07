54c32ec2678deb0c9bf113954e56a01bce9dd745
Eli Treuherz <eli@treuherz.co.uk>
2019-12-07 19:12:34 +0000

    2019 day 2 nicely

--- 19/src/bin/2.rs
+++ 19/src/bin/2.rs
@@ -2,12 +2,14 @@ use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Cursor, Read, Seek, SeekFrom};
 use std::io::SeekFrom::Start;
+use std::io::{BufRead, BufReader, Cursor, Read, Seek, SeekFrom};
 use std::ops::Deref;
 use std::str::FromStr;
 
 use aoc19::util::print_ans;
+use aoc19::computer;
+use std::hint::unreachable_unchecked;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -26,121 +28,26 @@ fn parse_commas(path: &str) -> Result<Vec<usize>> {
     Ok(out)
 }
 
-fn f1(inputs: &Vec<usize>) -> usize {
-    let mut mem = inputs.clone();
+fn f1(input: &Vec<usize>) -> usize {
+    let mut mem = input.clone();
     mem[1] = 12;
     mem[2] = 2;
-    let mut c = Computer {
-        mem: mem,
-    };
-    let out = c.run();
-    out[0]
+    computer::run(mem)[0]
 }
 
-//    let mut mem = inputs.to_owned();
-//    mem[1] = 12;
-//    mem[2] = 2;
-//
-//    let mut cur = 0;
-//    loop {
-//        match mem[cur] {
-//            99 => break,
-//            i @ 1 | i @ 2 => {
-//                cur += 1;
-//                let in1 = mem[cur];
-//                cur += 1;
-//                let in2 = mem[cur];
-//                cur += 1;
-//                let out = mem[cur];
-//                match i {
-//                    1 => mem[out] = mem[in1] + mem[in2],
-//                    2 => mem[out] = mem[in1] * mem[in2],
-//                    _ => unreachable!(),
-//                };
-//                cur += 1;
-//            }
-//            _ => panic!(),
-//        }
-//    }
-//
-//    mem[0]
-
-fn f2(inputs: &Vec<usize>) -> usize {
-    unimplemented!()
-}
-
-struct Computer {
-    mem: Vec<usize>,
-}
-
-impl Computer {
-    fn run(&self) -> Vec<usize> {
-        let mut mem = self.mem.clone();
-        let mut cur = 0;
-        loop {
-            match mem[cur] {
-                99 => break,
-                n @ _ => match Operation::from_code(&n) {
-                    None => panic!(n),
-                    Some(op) => {
-                        let (idx, val) = op.run(&cur, &mem);
-                        mem[idx] = val;
-                        cur += op.params() + 1;
-                    }
-                },
-            }
-        }
-        return mem;
-    }
-}
-
-enum Operation {
-    Add,
-    Mul,
-}
-
-impl Operation {
-    fn from_code(code: &usize) -> Option<Operation> {
-        use Operation::*;
-        match code {
-            1 => Some(Add),
-            2 => Some(Mul),
-            _ => None,
-        }
-    }
-
-    fn params(&self) -> usize {
-        use Operation::*;
-        match self {
-            Add => 3,
-            Mul => 3,
-        }
-    }
-
-    fn apply(&self, params: Vec<usize>, mem: &Vec<usize>) -> (usize, usize) {
-        match self {
-            Operation::Add => {
-                let (in1, in2, out) = (params[0], params[1], params[2]);
-                (out, mem[in1] + mem[in2])
-            }
-            Operation::Mul => {
-                let (in1, in2, out) = (params[0], params[1], params[2]);
-                (out, mem[in1] * mem[in2])
+fn f2(input: &Vec<usize>) -> usize {
+    const GOAL: usize = 19690720;
+    let max = input.len();
+    for noun in 0..max {
+        for verb in 0..max {
+            let mut mem = input.clone();
+            mem[1] = noun;
+            mem[2] = verb;
+            let out = computer::run(mem)[0];
+            if out == GOAL {
+                return 100 * noun + verb;
             }
         }
     }
-
-    fn read_params(&self, cur: &usize, mem: &Vec<usize>) -> Vec<usize> {
-        let count = self.params();
-        let mut v = Vec::with_capacity(count);
-        for i in 1..=count {
-            v.push(mem[cur + i]);
-        }
-        v
-    }
-
-    fn run(&self, cur: &usize, mem: &Vec<usize>) -> (usize, usize) {
-        let params = self.read_params(cur, mem);
-        self.apply(params, mem)
-    }
+    unreachable!()
 }
--- 19/src/lib.rs
+++ 19/src/lib.rs
@@ -22,3 +22,74 @@ pub mod util {
         println!("{}", f2(&inputs));
     }
 }
+
+pub mod computer {
+    pub fn run(mut mem: Vec<usize>) -> Vec<usize> {
+        let mut cur = 0;
+        loop {
+            match mem[cur] {
+                99 => break,
+                n @ _ => match Operation::from_code(&n) {
+                    None => panic!(n),
+                    Some(op) => {
+                        let (idx, val) = op.run(&cur, &mem);
+                        mem[idx] = val;
+                        cur += op.params() + 1;
+                    }
+                },
+            }
+        }
+        return mem;
+    }
+
+    enum Operation {
+        Add,
+        Mul,
+    }
+
+    impl Operation {
+        fn from_code(code: &usize) -> Option<Operation> {
+            use Operation::*;
+            match code {
+                1 => Some(Add),
+                2 => Some(Mul),
+                _ => None,
+            }
+        }
+
+        fn params(&self) -> usize {
+            use Operation::*;
+            match self {
+                Add => 3,
+                Mul => 3,
+            }
+        }
+
+        fn apply(&self, params: Vec<usize>, mem: &Vec<usize>) -> (usize, usize) {
+            match self {
+                Operation::Add => {
+                    let (in1, in2, out) = (params[0], params[1], params[2]);
+                    (out, mem[in1] + mem[in2])
+                }
+                Operation::Mul => {
+                    let (in1, in2, out) = (params[0], params[1], params[2]);
+                    (out, mem[in1] * mem[in2])
+                }
+            }
+        }
+
+        fn read_params(&self, cur: &usize, mem: &Vec<usize>) -> Vec<usize> {
+            let count = self.params();
+            let mut v = Vec::with_capacity(count);
+            for i in 1..=count {
+                v.push(mem[cur + i]);
+            }
+            v
+        }
+
+        fn run(&self, cur: &usize, mem: &Vec<usize>) -> (usize, usize) {
+            let params = self.read_params(cur, mem);
+            self.apply(params, mem)
+        }
+    }
+}
