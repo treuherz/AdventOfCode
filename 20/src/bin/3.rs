63fd62ebb3bf0ff548400065b5742bcc0a8da21a
Eli Treuherz <et@arenko.group>
2020-12-04 10:33:09 +0000

    2020 day 3

---
+++ 20/src/bin/3.rs
@@ -0,0 +1,80 @@
+use aoc20::util::{parse, print_answers};
+
+fn main() -> anyhow::Result<()> {
+    let inputs: Vec<String> = parse("inputs/3")?;
+    let map = Map::from_inputs(&inputs);
+    print_answers(&map, f1, f2);
+    Ok(())
+}
+
+const WIDTH: usize = 31;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Space {
+    Empty,
+    Tree,
+}
+
+type Line = [Space; WIDTH];
+type Coord = [usize; 2];
+
+struct Map {
+    data: Vec<Line>
+}
+
+impl Map {
+    fn from_inputs(inputs: &Vec<String>) -> Map {
+        let mut data: Vec<Line> = vec![];
+        for s in inputs {
+            let mut line = [Space::Empty; WIDTH];
+            for (x, c) in s.chars().enumerate() {
+                if c == '#' {
+                    line[x] = Space::Tree
+                }
+            }
+            data.push(line)
+        }
+        Map { data }
+    }
+
+    fn height(&self) -> usize {
+        self.data.len()
+    }
+
+    fn tree_at(&self, idx: Coord) -> bool {
+        self.data[idx[0]][idx[1] % WIDTH] == Space::Tree
+    }
+}
+
+fn f1(map: &Map) -> i64 {
+    let mut acc: i64 = 0;
+    let (mut x, mut y): (usize, usize) = (0, 0);
+    while y < map.height() {
+        if map.tree_at([y, x]) {
+            acc += 1
+        }
+        x += 3; y += 1
+    }
+    acc
+}
+
+fn del(y: usize, x: usize) -> impl Fn(Coord) -> Coord {
+   move |c| [c[0] + y, c[1] + x]
+}
+
+fn f2(map: &Map) -> i64 {
+    let mut acc: i64 = 1;
+    let slopes = vec![del(1, 1), del(1, 3), del(1, 5), del(1, 7), del(2, 1)];
+    for f in slopes {
+        let mut trees: i64 = 0;
+        let mut c: Coord = [0, 0];
+        while c[0] < map.height() {
+            if map.tree_at(c) {
+                trees += 1
+            }
+            c = f(c)
+        }
+        acc *= trees
+    }
+    acc
+}
--- 20/src/lib.rs
+++ 20/src/lib.rs
@@ -7,22 +7,22 @@ pub mod util {
         Ok(line_iter.collect())
     }
 
-    pub fn print_answers<I, O1, O2, F1, F2>(inputs: &Vec<I>, f1: F1, f2: F2)
+    pub fn print_answers<I, O1, O2, F1, F2>(input: &I, f1: F1, f2: F2)
     where
         O1: std::fmt::Display,
         O2: std::fmt::Display,
-        F1: Fn(&Vec<I>) -> O1,
-        F2: Fn(&Vec<I>) -> O2,
+        F1: Fn(&I) -> O1,
+        F2: Fn(&I) -> O2,
     {
-        println!("{}", f1(&inputs));
-        println!("{}", f2(&inputs));
+        println!("{}", f1(&input));
+        println!("{}", f2(&input));
     }
 
-    pub fn print_answer<I, O, F>(inputs: &Vec<I>, f: F)
+    pub fn print_answer<I, O, F>(input: &I, f: F)
     where
         O: std::fmt::Display,
-        F: Fn(&Vec<I>) -> O,
+        F: Fn(&I) -> O,
     {
-        println!("{}", f(&inputs));
+        println!("{}", f(&input));
     }
 }
