9b6e10530c25961be3c8ff4937943ad49240aaa5
Eli Treuherz <eli@treuherz.co.uk>
2020-12-15 09:21:04 +0000

    2020 14 wip

--- 20/src/bin/14.rs
+++ 20/src/bin/14.rs
@@ -1,68 +1,168 @@
-use std::convert::TryInto;
-use std::time::Instant;
-
 use bitvec::prelude::*;
-use itertools::Itertools;
-use nom::lib::std::str::FromStr;
 use regex::Regex;
+use std::{
+    collections::HashMap,
+    convert::TryInto,
+    ops::{BitAndAssign, BitOrAssign},
+    str::{from_utf8, FromStr},
+    time::Instant,
+};
 
-use anyhow::anyhow;
 use aoc20::util::{parse, print_answers};
-use std::str::from_utf8;
 
 fn main() -> anyhow::Result<()> {
     let now = Instant::now();
-    let inputs: Vec<String> = parse("inputs/14")?;
+    let inputs: Vec<Instruction> = parse("inputs/14")?;
     print_answers(14, &inputs, part1, part2);
     println!("Overall time: {:?}", now.elapsed());
     Ok(())
 }
 
-type Bits = bitarr!(for 36, in Msb0, u8);
+#[derive(Copy, Clone, Debug)]
+enum MaskValue {
+    Floating,
+    One,
+    Zero,
+}
 
 enum Instruction {
-    Mask { or: Bits, and: Bits },
+    Mask([MaskValue; 36]),
     Mem { address: u64, value: u64 },
 }
 
-impl FromStr for Instruction {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.starts_with("mask") {
-            let mask_str: &str = from_utf8(&s.as_bytes()[7..43])?;
-            let mut or_mask = bitarr![Msb0, u8; 0; 36];
-            let mut and_mask = bitarr![Msb0, u8; 1; 36];
-
-            for (i, c) in mask_str.char_indices() {
-                match c {
-                    'X' => continue,
-                    '0' => and_mask.set(i, false),
-                    '1' => or_mask.set(i, true),
-                    _ => return Err(anyhow!("unrecognised mask value")),
-                }
-            }
-
-            Ok(Instruction::Mask { or: or_mask, and: and_mask })
-        } else if s.starts_with("mem") {
-            let re = Regex::new(r"^mem\[(\d+)\] = (\d+)$").unwrap();
-            let caps = re.captures(s).unwrap();
-            let address: u64 = caps[1].parse()?;
-            let value: u64 = caps[2].parse()?;
-
-            Ok(Instruction::Mem { address, value })
-        } else {
-            Err(anyhow!("unrecognised instruction"))
+#[derive(thiserror::Error, Debug)]
+#[error("{msg}")]
+struct ParseError {
+    msg: String,
+}
+impl ParseError {
+    fn new(msg: &str) -> Self {
+        ParseError {
+            msg: msg.to_string(),
         }
     }
 }
 
-fn part1(inputs: &[String]) -> u64 {
-    todo!()
+impl FromStr for Instruction {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("mask") {
+            let mask_str: &str = from_utf8(&s.as_bytes()[7..43]).unwrap();
+            let mut mask = [MaskValue::Floating; 36];
+            for (i, c) in mask_str.char_indices() {
+                match c {
+                    'X' => continue,
+                    '0' => mask[36 - i - 1] = MaskValue::Zero,
+                    '1' => mask[36 - i - 1] = MaskValue::One,
+                    _ => return Err(ParseError::new("unrecognised mask value")),
+                }
+            }
+
+            Ok(Instruction::Mask(mask))
+        } else if s.starts_with("mem") {
+            let re = Regex::new(r"^mem\[(\d+)\] = (\d+)$").unwrap();
+            let caps = re.captures(s).unwrap();
+            let address: u64 = caps[1].parse().unwrap();
+            let value: u64 = caps[2].parse().unwrap();
+
+            Ok(Instruction::Mem { address, value })
+        } else {
+            Err(ParseError::new("unrecognised instruction"))
+        }
+    }
 }
 
-fn part2(inputs: &[String]) -> usize {
-    todo!()
+fn part1(inputs: &[Instruction]) -> u128 {
+    let mut map: HashMap<u64, u64> = HashMap::new();
+
+    let mut iter = inputs.iter();
+
+    let mut mask = if let Some(Instruction::Mask(mask)) = iter.next() {
+        mask
+    } else {
+        panic!("first element wasn't a mask")
+    };
+
+    for inst in iter {
+        match inst {
+            Instruction::Mask(new_mask) => {
+                mask = new_mask;
+            }
+            Instruction::Mem { address, value } => {
+                let mut bits: BitVec<Lsb0, u8> =
+                    value.view_bits::<Lsb0>().iter().take(36).collect();
+                bits = bits
+                    .iter()
+                    .zip(mask.iter())
+                    .map(|(&a, &b)| {
+                        match b {
+                            MaskValue::Floating => {a}
+                            MaskValue::One => {true}
+                            MaskValue::Zero => {false}
+                        }
+                    })
+                    .collect();
+                let mut bytes: [u8; 8] = [0; 8];
+                bytes
+                    .iter_mut()
+                    .zip(bits.as_slice())
+                    .for_each(|(target, byte)| *target = *byte);
+                let value = u64::from_le_bytes(bytes);
+                map.insert(*address, value);
+            }
+        }
+    }
+
+    let mut acc = 0u128;
+    map.values().for_each(|v| acc += *v as u128);
+    acc
+}
+
+fn part2(inputs: &[Instruction]) -> usize {
+    let mut map: HashMap<u64, u64> = HashMap::new();
+
+    let mut iter = inputs.iter();
+
+    let mut mask = if let Some(Instruction::Mask(mask)) = iter.next() {
+        mask
+    } else {
+        panic!("first element wasn't a mask")
+    };
+
+    for inst in iter {
+        match inst {
+            Instruction::Mask(new_mask) => {
+                mask = new_mask;
+            }
+            Instruction::Mem { address, value } => {
+                let mut bits: BitVec<Lsb0, u8> =
+                    address.view_bits::<Lsb0>().iter().take(36).collect();
+                bits = bits
+                    .iter_mut()
+                    .zip(mask.iter())
+                    .map(|(a, &b)| {
+                        match b {
+                            MaskValue::Floating => {a}
+                            MaskValue::One => {true}
+                            MaskValue::Zero => {false}
+                        }
+                    })
+                    .collect();
+                let mut bytes: [u8; 8] = [0; 8];
+                bytes
+                    .iter_mut()
+                    .zip(bits.as_slice())
+                    .for_each(|(target, byte)| *target = *byte);
+                let value = u64::from_le_bytes(bytes);
+                map.insert(*address, value);
+            }
+        }
+    }
+
+    let mut acc = 0u128;
+    map.values().for_each(|v| acc += *v as u128);
+    acc
 }
 
 #[cfg(test)]
@@ -71,7 +171,7 @@ mod tests {
 
     #[test]
     fn bit_assign() {
-        let mut store = bitarr![Msb0, u8; 0; 36];
+        let mut store = bitarr![Lsb0, u8; 0; 36];
         dbg!(store);
     }
 
