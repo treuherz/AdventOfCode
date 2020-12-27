b25a24aed4c3e4ad8cdc4118d4fcb465ecffc954
Eli Treuherz <eli@treuherz.co.uk>
2020-12-27 14:33:01 +0000

    2020 day 23 part 2 improved

--- 20/src/bin/23.rs
+++ 20/src/bin/23.rs
@@ -1,6 +1,5 @@
 use aoc20::util::print_answers;
-use itertools::Itertools;
-use std::{collections::VecDeque, time::Instant};
+use std::{fmt::Write, time::Instant};
 
 fn main() -> anyhow::Result<()> {
     let now = Instant::now();
@@ -13,80 +12,115 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn play(mut ring: VecDeque<usize>, moves: i32) -> VecDeque<usize> {
-    let min = *ring.iter().min().unwrap();
-    let max = *ring.iter().max().unwrap();
+struct Node {
+    prev: usize,
+    next: usize,
+}
 
-    ring.rotate_left(1);
+struct Ring {
+    head: usize,
+    arena: Vec<Node>,
+}
 
-    let mut t = Instant::now();
-    let mut tt = Instant::now();
-    for i in 0..moves {
-        if i % 10_000 == 0 {
-            dbg!(i, t.elapsed());
-            t = Instant::now();
-            tt = Instant::now();
-        };
+impl Ring {
+    fn new(data: &[usize], max: usize) -> Self {
+        let mut arena = Vec::with_capacity(data.len().max(max) + 1);
+        // Placeholder node to make indexes line up. Gross.
+        arena.push(Node { prev: 0, next: 0 });
+        assert!(data.len() >= 3);
+        let data_max = *data.iter().max().unwrap();
 
-        let mut held = VecDeque::with_capacity(3);
-        (0..3).for_each(|_| held.push_back(ring.pop_front().unwrap()));
-
-        let cur = *ring.back().unwrap();
-        let mut dst = if cur == min { max } else { cur - 1 };
-
-        let dst_pos = loop {
-            if held.contains(&dst) {
-                if dst == min {
-                    dst = max;
-                } else {
-                    dst -= 1;
-                }
+        for i in 1..=max {
+            if i <= data_max {
+                let pos = data.iter().position(|&n| n == i).unwrap();
+                let prev = data[if pos == 0 { data.len() - 1 } else { pos - 1 }];
+                let next = data[if pos == data.len() - 1 { 0 } else { pos + 1 }];
+                arena.push(Node { prev, next })
             } else {
-                if i % 10_000 == 0 {
-                    dbg!(tt.elapsed());
-                    tt = Instant::now();
-                };
-                break ring.iter().rposition(|&n| n == dst).unwrap();
+                arena.push(Node {
+                    prev: i - 1,
+                    next: i + 1,
+                })
+            }
+        }
+
+        if max > data_max {
+            let data_first = *data.first().unwrap();
+            let data_last = *data.last().unwrap();
+            let generated_first = data_max + 1;
+            arena[max].next = data_first;
+            arena[data_first].prev = max;
+            arena[data_last].next = generated_first;
+            arena[generated_first].prev = data_last;
+        }
+
+        Ring {
+            arena,
+            head: data[0],
+        }
+    }
+}
+
+fn play(mut ring: Ring, moves: i32) -> Ring {
+    let min = 1;
+    let max = ring.arena.len() - 1;
+
+    for _ in 0..moves {
+        let mut held = Vec::with_capacity(3);
+        let mut idx = ring.arena[ring.head].next;
+        for _ in 0..3 {
+            held.push(idx);
+            idx = ring.arena[idx].next;
+        }
+        ring.arena[ring.head].next = idx;
+
+        let mut n = ring.head;
+        let dst = loop {
+            n = if n == min { max } else { n - 1 };
+            if !held.contains(&n) {
+                break n;
             }
         };
-        if i % 10_000 == 0 {
-            dbg!(dst_pos, tt.elapsed());
-            tt = Instant::now();
-        };
 
-        ring.rotate_left(dst_pos+1);
-        (0..3).for_each(|_| ring.push_front(held.pop_back().unwrap()));
-        ring.rotate_right(dst_pos);
-        if i % 10_000 == 0 {
-            dbg!(tt.elapsed());
-            tt = Instant::now();
-        };
+        let dst_next = ring.arena[dst].next;
+
+        let mut idx = dst;
+        for n in held {
+            ring.arena[idx].next = n;
+            ring.arena[n].prev = idx;
+            idx = n;
+        }
+        ring.arena[idx].next = dst_next;
+        ring.arena[dst_next].prev = idx;
+
+        ring.head = ring.arena[ring.head].next;
     }
 
     ring
 }
 
 fn part1(cups: &[usize]) -> String {
-    let ring: VecDeque<usize> = cups.iter().copied().collect();
+    let ring = Ring::new(cups, 9);
 
-    let mut ring = play(ring, 100);
+    let ring = play(ring, 100);
 
-    while ring[0] != 1 {
-        ring.rotate_right(1);
+    let mut out = String::new();
+    let mut idx = 1;
+    while ring.arena[idx].next != 1 {
+        write!(out, "{}", ring.arena[idx].next).unwrap();
+        idx = ring.arena[idx].next;
     }
-    ring.iter().skip(1).map(|n| n.to_string()).join("")
+    out
 }
 
 fn part2(cups: &[usize]) -> usize {
-    let max = *cups.iter().max().unwrap();
-    let ring: VecDeque<usize> = cups.iter().copied().chain((max + 1)..=1_000_000).collect();
+    let ring = Ring::new(cups, 1_000_000);
 
-    let mut ring = play(ring, 10_000_000);
+    let ring = play(ring, 10_000_000);
 
-    while ring[0] != 1 {
-        ring.rotate_right(1);
-    }
-    ring.iter().skip(1).take(2).product()
+    let a = ring.arena[1].next;
+    let b = ring.arena[a].next;
+    a * b
 }
 
 #[cfg(test)]
@@ -101,5 +135,15 @@ mod tests {
             .collect();
 
         assert_eq!(super::part1(&cups), "67384529".to_string());
+    }
+
+    #[test]
+    fn part2() {
+        let cups: Vec<usize> = "389125467"
+            .chars()
+            .map(|c| c.to_string().parse().unwrap())
+            .collect();
+
+        assert_eq!(super::part2(&cups), 149245887792);
     }
 }
