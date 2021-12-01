b6e2f05985015a4fee3898a04da9b3e75b0b7a9c
Eli Treuherz <et@arenko.group>
2021-12-01 13:08:19 +0000

    2021 Day 1

---
+++ 21/src/day1.clj
@@ -0,0 +1,20 @@
+(ns day1
+  (:require [clojure.string :as str]))
+
+(def input (map #(Integer/parseInt %) (str/split-lines (slurp (clojure.java.io/resource "1.txt")))))
+
+(defn part1 [input] (let
+                      [pairs (partition 2 1 input)]
+                      (count (filter identity
+                                    (for [[a b] pairs] (< a b))))))
+
+(defn part2 [input] (let
+                      [runs (partition 3 1 input)
+                       sums (map #(apply + %) runs)
+                       pairs (partition 2 1 sums)]
+                      (count (filter identity
+                                    (for [[a b] pairs] (< a b))))))
+
+(defn -main [] (do
+                 (println "Part 1:" (part1 input))
+                 (println "Part 2:" (part2 input))))
