829e7999356f94fec97eb685e95195c6148a2946
Eli Treuherz <et@arenko.group>
2021-12-02 09:36:18 +0000

    2021 Day 1: concision

--- 21/src/day1.clj
+++ 21/src/day1.clj
@@ -3,18 +3,17 @@
 
 (def input (map #(Integer/parseInt %) (str/split-lines (slurp (clojure.java.io/resource "1.txt")))))
 
-(defn part1 [input] (let
-                      [pairs (partition 2 1 input)]
-                      (count (filter identity
-                                    (for [[a b] pairs] (< a b))))))
+(defn part1
+  [input] (let [pairs (partition 2 1 input)]
+            (count (filter #(let [[a b] %] (< a b)) pairs))))
 
-(defn part2 [input] (let
-                      [runs (partition 3 1 input)
-                       sums (map #(apply + %) runs)
-                       pairs (partition 2 1 sums)]
-                      (count (filter identity
-                                    (for [[a b] pairs] (< a b))))))
+(defn part2
+  [input] (let [runs (partition 3 1 input)
+                sums (map #(apply + %) runs)
+                pairs (partition 2 1 sums)]
+            (count (filter #(let [[a b] %] (< a b)) pairs))))
 
-(defn -main [] (do
-                 (println "Part 1:" (part1 input))
-                 (println "Part 2:" (part2 input))))
+(defn -main
+  [] (do
+       (println "Part 1:" (part1 input))
+       (println "Part 2:" (part2 input))))
