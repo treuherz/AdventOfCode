6b3d7c9da9fe0c5d5889b9c4b7efcb152df52c80
Eli Treuherz <et@arenko.group>
2021-12-03 09:37:59 +0000

    2021: Formatting

--- 21/src/day1.clj
+++ 21/src/day1.clj
@@ -1,24 +1,29 @@
 (ns day1
   (:require [clojure.string :as str]))
 
-(defn parse [file] (map #(Integer/parseInt %) (str/split-lines (slurp (clojure.java.io/resource file)))))
+(defn parse
+  (->> file
+    (clojure.java.io/resource)
+    (slurp)
+    (str/split-lines)
+    (map #(Integer/parseInt %))))
 
-(defn part1
-  [input] (->> input
-            (partition 2 1)
-            (filter #(let [[a b] %] (< a b)))
-            (count)))
+(defn part1 [input]
+  (->> input
+    (partition 2 1)
+    (filter #(let [[a b] %] (< a b)))
+    (count)))
 
 
-(defn part2
-  [input] (->> input
-            (partition 3 1)
-            (map #(apply + %))
-            (partition 2 1)
-            (filter #(let [[a b] %] (< a b)))
-            (count)))
+(defn part2 [input]
+  (->> input
+    (partition 3 1)
+    (map #(apply + %))
+    (partition 2 1)
+    (filter #(let [[a b] %] (< a b)))
+    (count)))
 
-(defn -main
-  [] (do (let [input (parse "1.txt")]
-       (println "Part 1:" (part1 input))
-       (println "Part 2:" (part2 input)))))
+(defn -main []
+  (let [input (parse "1.txt")]
+    (println "Part 1:" (part1 input))
+    (println "Part 2:" (part2 input))))
--- 21/src/day2.clj
+++ 21/src/day2.clj
@@ -1,38 +1,34 @@
 (ns day2
   (:require [clojure.string :as str]))
 
-(defn parse
-  [file] (->> file
-           (clojure.java.io/resource)
-           (slurp)
-           (str/split-lines)
-           (map #(str/split % #"\s" 2))
-           (map (fn [[dir n-str]] {:dir dir, :n (Integer/parseInt n-str)}))))
+(defn parse [file]
+  (->> file
+    (clojure.java.io/resource)
+    (slurp)
+    (str/split-lines)
+    (map #(str/split % #"\s" 2))
+    (map (fn [[dir n-str]] {:dir dir, :n (Integer/parseInt n-str)}))))
 
-(defn f1
-  [{dir :dir, n :n}
-   [pos, depth]]
+(defn f1 [{dir :dir, n :n} [pos depth]]
   (condp = dir
     "forward" [(+ pos n) depth]
     "up" [pos (- depth n)]
     "down" [pos (+ depth n)]))
 
-(defn f2
-  [{dir :dir, n :n}
-   [pos, depth, aim]]
+(defn f2 [{dir :dir, n :n} [pos depth aim]]
   (condp = dir
     "forward" [(+ pos n) (+ depth (* aim n)) aim]
     "up" [pos depth (- aim n)]
     "down" [pos depth (+ aim n)]))
 
-(defn run
-  [input start move] (loop [[head & rest] input
-                            coords start]
-                       (if (nil? head)
-                         (* (first coords) (second coords))
-                         (recur rest (move head coords)))))
+(defn run [input start move]
+  (loop [[head & rest] input
+         coords start]
+    (if (nil? head)
+      (* (first coords) (second coords))
+      (recur rest (move head coords)))))
 
-(defn -main
-  [] (do (let [input (parse "2.txt")]
-           (println "Part 1:" (run input [0 0] f1))
-           (println "Part 2:" (run input [0 0 0] f2)))))
+(defn -main []
+  (let [input (parse "2.txt")]
+    (println "Part 1:" (run input [0 0] f1))
+    (println "Part 2:" (run input [0 0 0] f2))))
