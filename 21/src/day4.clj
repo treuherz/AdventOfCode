31f92435af55d9b5744dceaa8c13d89e64f70374
Eli Treuherz <et@arenko.group>
2021-12-04 12:22:48 +0000

    2021 Day 2 Part 2

--- 21/src/day4.clj
+++ 21/src/day4.clj
@@ -28,23 +28,41 @@
 
 (defn bingo? [marks] (boolean (some #(= 5 (count %)) marks)))
 
-(defn positions [pred coll] (keep-indexed (fn [i v] (when (pred v) i)) coll))
+(defn first-index [pred coll] (first (keep-indexed (fn [i v] (if (pred v) i)) coll)))
 
 (defn empty-marks [n size] (for [_ (range n)] (vec (for [_ (range size)] []))))
 
-(defn part1 [{draws :draws, boards :boards}]
+(defn bingo [draws boards]
   ;; So we only have to test horizontal lines, we duplicate and transpose the boards
   (let [all-boards (concat boards (map (partial apply mapv vector) boards))]
     (loop [[drawn & rest] draws
            ;; nested vectors of marked numbers
-           marked (empty-marks (count boards) 5)]
-      (let [marked (mark drawn all-boards marked)]
-        ;; get the index of the board for which any row has 5 marks
-        (if-let [idx (first (positions bingo? marked))]
-          (score (nth all-boards idx) (nth marked idx) drawn)
-          (recur rest marked))))))
+           marked (empty-marks (count all-boards) 5)]
+      (if (nil? drawn)
+        nil
+        (let [marked (mark drawn all-boards marked)]
+          ;; get the index of the board for which any row has 5 marks
+          (if-let [idx (first-index bingo? marked)]
+            {:idx   (mod idx (count boards))                ; we duplicated all the boards, now must undo
+             :score (score (nth all-boards idx) (nth marked idx) drawn)}
+            (recur rest marked)))))))
 
-(defn part2 [input] "-")
+(defn part1 [{draws :draws, boards :boards}] (:score (bingo draws boards)))
+
+(defn drop-nth [n coll] (keep-indexed #(if (not= n %1) %2) coll))
+
+(defn part2 [{draws :draws, init-boards :boards}]
+  ;; Run the game again and again, removing the winning board each time
+  ;; This is inefficient but means I don't need to rip the bingo impl open
+  (loop [boards init-boards
+         last-win nil]
+    (if (empty? boards)
+      ;; No more boards, return last win
+      (:score last-win)
+      (if-let [win (bingo draws boards)]
+        (recur (drop-nth (:idx win) boards) win)
+        ;; No more winners, return last win
+        (:score last-win)))))
 
 (defn -main []
   (let [input (trace "Input" (parse "4.txt"))]
