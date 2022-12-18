8a0f52a54794dd631a2765e60292f2274a1f10ed
Eli Treuherz <eli@treuherz.co.uk>
2022-12-18 13:26:50 +0000

    2022 days 10, 11

---
+++ 22/src/aoc22/day10.clj
@@ -0,0 +1,103 @@
+(ns aoc22.day10
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]
+   [clojure.tools.trace :refer [trace deftrace]]))
+
+(def arg-keys
+  (->> (iterate inc 1)
+       (map str)
+       (map keyword)))
+
+(defn parse-instruction [[op & args]]
+  (apply merge {:op (keyword op)} (zipmap arg-keys (map parse-long args))))
+
+(defn parse [s]
+  (->> s
+       str/split-lines
+       (map #(str/split % #" "))
+       (map parse-instruction)))
+
+(defn record-if [at cycle coll x]
+  (if (contains? at cycle)
+    (conj coll x)
+    coll))
+
+(defn instruction-duration [instruction]
+  (condp = (:op instruction)
+    :noop 1
+    :addx 2))
+
+(defn record [recorded record-cycle cycle duration x]
+  (loop [n        0
+         recorded recorded]
+    (if (< n duration)
+      (let [cycle         (+ cycle n)
+            should-record (record-cycle cycle)
+            recorded      (if should-record (conj recorded (* cycle x)) recorded)]
+        (recur (inc n) recorded))
+      recorded)))
+
+
+(defn run [instructions record-cycle]
+  (loop [cycle        1
+         x            1
+         instructions instructions
+         recorded     []]
+    (if-let [[instruction & instructions] (seq instructions)]
+      (let [duration   (instruction-duration instruction)
+            next-cycle (+ cycle duration)
+            recorded   (record recorded record-cycle cycle duration x)]
+        (condp = (:op instruction)
+          :noop (recur next-cycle x instructions recorded)
+          :addx (recur next-cycle (+ x (:1 instruction)) instructions recorded)))
+      recorded)))
+
+(defn part1 [input]
+  (reduce + (run input #(= (mod % 40) 20))))
+
+(defn add-pixels [screen start-cycle duration x]
+  (loop [cycle  start-cycle
+         screen screen]
+    (if (< cycle (+ start-cycle duration))
+      (let [sprite  (set [(dec x) x (inc x)])
+            lit     (contains? sprite (mod (dec cycle) 40))
+            pixel   (if lit \# \.)
+            screen  (str screen pixel)]
+        (recur (inc cycle) screen))
+      screen)))
+
+(defn apply-instruction [{op :op, arg1 :1} x]
+  (condp = op
+    :noop x
+    :addx (+ x arg1)))
+
+(defn part2 [input]
+  (loop [cycle        1
+         x            1
+         instructions input
+         screen       ""]
+    (if-let [[i & instructions] (seq instructions)]
+      (let [duration   (instruction-duration i)
+            next-cycle (+ cycle duration)
+            screen     (add-pixels screen cycle duration x)
+            x          (apply-instruction i x)]
+        (recur next-cycle x instructions screen))
+      (str/join "\n" (trace (map str/join (partition 40 screen)))))))
+
+(deftest tests
+  (let [sample1 "noop\naddx 3\naddx -5"
+        sample2 "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop"
+        input1  (parse sample1)
+        input2  (parse sample2)]
+    (is (= input1 [{:op :noop} {:op :addx, :1 3} {:op :addx, :1 -5}]) "parse sample1")
+    (is (= (part1 input2) 13140) "part1")
+    (is (= (part2 input2) "##..##..##..##..##..##..##..##..##..##..\n###...###...###...###...###...###...###.\n####....####....####....####....####....\n#####.....#####.....#####.....#####.....\n######......######......######......####\n#######.......#######.......#######.....") "part2")))
+
+(defn -main []
+  (let [file  (slurp (io/resource "10.txt"))
+        input (parse file)]
+    (println "Part 1:" (part1 input))
+    (println "Part 2:")
+    (println (part2 input))))
---
+++ 22/src/aoc22/day11.clj
@@ -0,0 +1,92 @@
+(ns aoc22.day11
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn split-linegroups [s] (str/split s #"\r?\n\r?\n"))
+
+(defn parse-monkey [s]
+  (let [re #"Monkey (?<monkey>\d+):\n\s+Starting items: (?<items>[\d, ]+)\n\s+Operation: new = old (?<op>[*+]) (?<operand>[\w]+)\n\s+Test: divisible by (?<div>\d+)\n\s+If true: throw to monkey (?<iftrue>\d+)\n\s+If false: throw to monkey (?<iffalse>\d+)"
+        m  (re-matcher re s)]
+    (when (re-find m)
+      {:monkey   (parse-long (.group m "monkey"))
+       :items    (mapv parse-long (str/split (.group m "items") #", "))
+       :op       (condp = (.group m "op")
+                   "*" *
+                   "+" +)
+       :operand  (if-some [n (parse-long (.group m "operand"))] n :old)
+       :div      (parse-long (.group m "div"))
+       :if-true  (parse-long (.group m "iftrue"))
+       :if-false (parse-long (.group m "iffalse"))})))
+
+(defn parse [s]
+  (as-> s $
+        (split-linegroups $)
+        (map parse-monkey $)
+        (into {} (for [m $] [(:monkey m) m]))))
+
+(defn monkey-modulo [monkeys]
+  (reduce * (map :div (vals monkeys))))
+
+(defn monkey-business [monkeys]
+  (->> monkeys
+       vals
+       (map :n)
+       sort
+       reverse
+       (take 2)
+       (reduce *)))
+
+(defn inspect [{op :op, operand :operand, divisor :div, if-true :if-true, if-false :if-false} old relief modulo]
+  (let [operand  (if (= operand :old) old operand)
+        worried  (op old operand)
+        relieved (relief (mod worried modulo))
+        test     (zero? (mod relieved divisor))
+        target   (if test if-true if-false)]
+    [target relieved]))
+
+(defn run [input rounds relief]
+  (let [modulo (monkey-modulo input)]
+    (loop [round-idx  0
+           monkey-idx 0
+           monkeys    (into {} (for [[m monkey] input] [m (assoc monkey :n 0)]))]
+      (if (< round-idx rounds)
+        (if (< monkey-idx (count monkeys))
+          (let [monkey         (get monkeys monkey-idx)
+                inspected      (for [item (:items monkey)] (inspect monkey item relief modulo))
+                target-monkeys (map first inspected)
+                target-items   (into {} (for [m target-monkeys] [m (map second (filter #(= (first %) m) inspected))]))
+                n              (+ (:n monkey) (count inspected))
+                monkeys        (into {} (for [[m monkey] monkeys
+                                              :let [items     (:items monkey)
+                                                    receiving (get target-items m)]]
+                                          [m
+                                           (cond
+                                             (= monkey-idx m) (assoc monkey :n n :items [])
+                                             receiving (assoc monkey :items (concat items receiving))
+                                             :else monkey)]))]
+            (recur round-idx (inc monkey-idx) monkeys))
+          (recur (inc round-idx) 0 monkeys))
+        (monkey-business monkeys)))))
+
+(defn part1 [input]
+  (run input 20 #(quot % 3)))
+
+(defn part2 [input]
+  (run input 10000 identity))
+
+(deftest tests
+  (let [sample1 "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1"
+        input1  (parse sample1)]
+    (is (= (get input1 0) {:monkey 0, :items [79, 98], :op *, :operand 19, :div 23, :if-true 2, :if-false 3}) "parse sample1")
+    (is (= (monkey-business {0 {:n 101}, 1 {:n 95}, 2 {:n 7}, 3 {:n 105}}) 10605))
+    (is (= (into {} [[:a 1]])))
+    (is (= (part1 input1) 10605) "part1")
+    (is (= (part2 input1) 2713310158) "part2")))
+
+(defn -main []
+  (let [file  (slurp (io/resource "11.txt"))
+        input (parse file)]
+    (println "Part 1:" (part1 input))
+    (println "Part 2:" (part2 input))))
